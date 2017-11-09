d207a0d9cd15db268836c14826a999db87696985
gracecuenca <gcuenca@stuy.edu>
2017-11-09 08:33:57 -0500

    actually read hw directions

--- signal.c
+++ signal.c
@@ -2,14 +2,22 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <string.h>
 
 static void sighandler(int signo){
+  int fd;
+  char * message = "exited due to sigint\n";
   if(signo == SIGINT){
+    fd = open("file", O_CREAT | O_WRONLY | O_APPEND, 0644);
+    write(fd,&message, strlen(message));
     printf("exited due to sigint\n");
+    close(fd);
     exit(0);
   }
   if(signo==SIGUSR1){
-    printf("pid: %d\n",getpid());
+    printf("parent pid: %d\n",getppid());
   }
 }
 
@@ -18,8 +26,7 @@ int main(){
   signal(SIGUSR1, sighandler);
 
   while(1){
-    int pid = getpid();
-    kill(pid, SIGUSR1);
+    printf("pid: %d\n",getpid());
     sleep(1);
   }
 }
